8bf1351186c15f358a820b936c370fadf7a1f70d
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2022-11-10 17:41:26 +1100

    cleaning up doom.js file

--- doom.js
+++ doom.js
@@ -1,30 +1,10 @@
 //Js file for Doom
 /*
--_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-| Doom Definitions
-| The following lines of code are ported from the doomdef.h and doomdef.c
-| These lines define the global data structures used across other sections of the game
--_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+| Shared Classes																				|
+| The following lines of code are classes made in js to abstract/represent c data types			|
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 */
-//Below declares all static variables that are declared in the doomdef file
-var VERSION = 110;
-var TICRATE = 35;
-var INVULNTICS = (30*TICRATE);
-var INVISTICS = (60*TICRATE);
-var INFRATICS = (120*TICRATE);
-var IRONTICS = (60*TICRATE);
-var BASE_WIDTH = 320;
-var SCREEN_MUL = 1;
-var INV_ASPECT_RATIO = 0.625;
-var SCREENWIDTH = 320;
-var SCREENHEIGHT = 200;
-var MAXPLAYERS = 4;
-var MTF_EASY = 1;
-var MTF_NORMAL = 2;
-var MTF_HARD = 4;
-var MTF_AMBUSH = 8;
-
-
 //Below are the 2 prototype classes for enums in javascript 
 //the enum_tProto class lays out the basic functionality of the class, getter and setter for the value and declares the list of potential values
 //the enum_t class acts as a middle man between enum_tProto and every child class of enum_t, it simplifies the interface and lets future class definitions get away with 3 lines
@@ -52,6 +32,32 @@ class enum_t extends enum_tProto {
 		this.val = inputArgument;
 	}
 }
+
+/*
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+| Doom Definitions																				|
+| The following lines of code are ported from the doomdef.h and doomdef.c						|
+| These lines define the global data structures used across other sections of the game			|
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+*/
+//Below declares all static variables that are declared in the doomdef file
+var VERSION = 110;
+var TICRATE = 35;
+var INVULNTICS = (30*TICRATE);
+var INVISTICS = (60*TICRATE);
+var INFRATICS = (120*TICRATE);
+var IRONTICS = (60*TICRATE);
+var BASE_WIDTH = 320;
+var SCREEN_MUL = 1;
+var INV_ASPECT_RATIO = 0.625;
+var SCREENWIDTH = 320;
+var SCREENHEIGHT = 200;
+var MAXPLAYERS = 4;
+var MTF_EASY = 1;
+var MTF_NORMAL = 2;
+var MTF_HARD = 4;
+var MTF_AMBUSH = 8;
+
 
 //Below declares all the classes for the enums in doomdef
 class GameMode_t extends enum_t {
@@ -102,3 +108,10 @@ class ammotype_t extends enum_t {
 	}
 }
 
+/*
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+| Doom Data																						|
+| The following lines of code are ported from the doomdata.h									|
+| These lines define the global data structures used across other sections of the game			|
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+*/
